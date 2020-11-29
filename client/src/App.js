7428d2293e26fb6d80b3666552e62d5f34fdcb7d
Straltsou Maxim <green1971weekend1@gmail.com>
2020-11-29 21:06:53 +0300

    feat: loader component added

--- client/src/App.js
+++ client/src/App.js
@@ -4,14 +4,19 @@ import {BrowserRouter as Router} from "react-router-dom";
 import { useAuth } from "./hooks/auth.hook";
 import { AuthContext } from "./context/AuthContext";
 import { Navbar } from "./components/Navbar";
+import { Loader } from "./components/Loader";
+
 import "materialize-css";
 
 
 function App() {
-  const {token, login, logout, userId} = useAuth();
+  const {token, login, logout, userId, ready} = useAuth();
   const isAuthenticated = !!token;  // 2 exclamation points is cast to boolean type
   const routes = useRoutes(isAuthenticated);
  
+  if(!ready) {
+    return <Loader/>
+  }
   return (
     <AuthContext.Provider value={{
       token, login, logout, userId, isAuthenticated
