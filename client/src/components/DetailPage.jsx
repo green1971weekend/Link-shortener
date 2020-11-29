69c0d061b6787caff50de324a9e42cf36e023664
Straltsou Maxim <green1971weekend1@gmail.com>
2020-11-29 21:33:00 +0300

    feat: LinkCard component added

--- client/src/components/DetailPage.jsx
+++ client/src/components/DetailPage.jsx
@@ -33,7 +33,7 @@ export const DetailPage = () => {
 
     return(
         <>
-            { !loading && link && <LinkCard />}
+            { !loading && link && <LinkCard link={link}/>}
         </>
     );
 }
